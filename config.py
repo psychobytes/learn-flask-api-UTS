e7944f693391da8e67304b4a19ddd8791f107546
psychobytes <45039854+psychobytes@users.noreply.github.com>
2024-11-14 22:25:31 +0700

    Update config.py
    update db connection

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # Konfigurasi MySQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://learnflaskapiuts_zulunicego:b0406f6eaa7838b395266a384309d56eba4f9e95@0ug9z.h.filess.io/learnflaskapiuts_zulunicego'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://learnflaskapiuts_zulunicego:b0406f6eaa7838b395266a384309d56eba4f9e95@0ug9z.h.filess.io:3305/learnflaskapiuts_zulunicego'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
